c7c37b77309a829c47d6842c4ba2ab4aecc69a0f
Matheus G. Costa | Dev <matheuscosta.emilio@gmail.com>
2026-04-24 13:47:18 -0300

    Estrutura inicial da API

---
+++ src/main/java/com/matheus/loja/api/entity/Produto.java
@@ -0,0 +1,52 @@
+package com.matheus.loja.api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Double preco;
+
+    @Column(nullable = false)
+    private Integer estoque;
+
+    public Produto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
